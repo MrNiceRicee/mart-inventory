96e44c6715e20b8d8b423565b59ac0933b2c0905
Stoyko Tolev <contact@stoykotolev.com>
2022-12-21 23:40:16 +0200

    feat(autocomplete.tsx index.tsx): add autocomplete field. Add RHF
    Create a generic autocomplete field. Install RHF.

---
+++ src/components/Autocomplete.tsx
@@ -0,0 +1,129 @@
+import { Combobox, Transition } from "@headlessui/react";
+import { Fragment, useState } from "react";
+
+type BaseDataType<T> = T & { id: string; name: string };
+type AutocompleteComponentType<T> = {
+  options: BaseDataType<T>[];
+  label: string;
+  name: string;
+  selected: BaseDataType<T> | undefined;
+  onChange: (value: T) => void;
+};
+
+export const Autocomplete = <T,>({
+  options,
+  label,
+  name,
+  selected,
+  onChange,
+}: AutocompleteComponentType<T>) => {
+  const [query, setQuery] = useState("");
+
+  const filteredData =
+    query === ""
+      ? options
+      : options.filter((item) =>
+          item.name
+            .toLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLowerCase().replace(/\s+/g, ""))
+        );
+
+  return (
+    <div className="flex items-center justify-center p-12">
+      <div className="fixed top-16 w-72">
+        <Combobox nullable name={name} onChange={onChange} value={selected}>
+          <Combobox.Label>{label}</Combobox.Label>
+          <div className="relative mt-1">
+            <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
+              <Combobox.Input
+                className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
+                displayValue={(value: BaseDataType<T>) => value?.name}
+                onChange={(event) => setQuery(event.target.value)}
+              />
+              <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
+                <svg
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="h-6 w-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+                  />
+                </svg>
+              </Combobox.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              leave="transition ease-in duration-100"
+              leaveFrom="opacity-100"
+              leaveTo="opacity-0"
+              afterLeave={() => setQuery("")}
+            >
+              <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {filteredData?.length === 0 && query !== "" ? (
+                  <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
+                    Nothing found.
+                  </div>
+                ) : (
+                  filteredData?.map((person) => (
+                    <Combobox.Option
+                      key={person.id}
+                      className={({ active }) =>
+                        `relative cursor-default select-none py-2 pl-10 pr-4 ${
+                          active ? "bg-teal-600 text-white" : "text-gray-900"
+                        }`
+                      }
+                      value={person}
+                    >
+                      {({ selected, active }) => (
+                        <>
+                          <span
+                            className={`block truncate ${
+                              selected ? "font-medium" : "font-normal"
+                            }`}
+                          >
+                            {person.name}
+                          </span>
+                          {selected ? (
+                            <span
+                              className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                                active ? "text-white" : "text-teal-600"
+                              }`}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="h-6 w-6"
+                                aria-hidden="true"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M4.5 12.75l6 6 9-13.5"
+                                />
+                              </svg>
+                            </span>
+                          ) : null}
+                        </>
+                      )}
+                    </Combobox.Option>
+                  ))
+                )}
+              </Combobox.Options>
+            </Transition>
+          </div>
+        </Combobox>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,10 @@ import { type NextPage } from "next";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 import { trpc } from "../utils/trpc";
-import { Combobox, Transition } from "@headlessui/react";
-import { Fragment, useEffect, useState } from "react";
-import { Category } from "@prisma/client";
-
-type Person = {
-  id: number;
-  name: string;
-};
+import { useEffect, useState } from "react";
+import { Autocomplete } from "../components/Autocomplete";
+import { type Category } from "@prisma/client";
+import { useForm } from "react-hook-form";
 
 const Home: NextPage = () => {
   const { data: categoriesData, isLoading: categoriesLoading } =
@@ -17,7 +13,18 @@ const Home: NextPage = () => {
       text: "askldfsalkdfhasdklfakshdfsadf",
     });
 
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = (data) => console.log(data);
+
   const [localCategories, setLocalCategories] = useState<Category[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<
+    Category | undefined
+  >();
 
   useEffect(() => {
     if (!categoriesLoading && categoriesData) {
@@ -25,18 +32,9 @@ const Home: NextPage = () => {
     }
   }, [categoriesLoading, categoriesData]);
 
-  const [selected, setSelected] = useState<Category | undefined>();
-  const [query, setQuery] = useState("");
-
-  const filteredcategory =
-    query === ""
-      ? localCategories
-      : localCategories.filter((person) =>
-          person.name
-            .toLowerCase()
-            .replace(/\s+/g, "")
-            .includes(query.toLowerCase().replace(/\s+/g, ""))
-        );
+  const handleCategoryChange = (value: Category) => {
+    setSelectedCategory(value);
+  };
 
   return categoriesLoading ? (
     <div>Loading...</div>
@@ -44,97 +42,17 @@ const Home: NextPage = () => {
     <div className="h-screen bg-gray-700">
       <div className="flex items-center justify-center p-12">
         <div className="fixed top-16 w-72">
-          <Combobox value={selected} onChange={setSelected}>
-            <Combobox.Label>Assignee:</Combobox.Label>
-            <div className="relative mt-1">
-              <div className="relative w-full cursor-default overflow-hidden rounded-lg bg-white text-left shadow-md focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-teal-300 sm:text-sm">
-                <Combobox.Input
-                  className="w-full border-none py-2 pl-3 pr-10 text-sm leading-5 text-gray-900 focus:ring-0"
-                  displayValue={(person: Person) => person.name}
-                  onChange={(event) => setQuery(event.target.value)}
-                />
-                <Combobox.Button className="absolute inset-y-0 right-0 flex items-center pr-2">
-                  <svg
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth={1.5}
-                    stroke="currentColor"
-                    className="h-6 w-6"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-                    />
-                  </svg>
-                </Combobox.Button>
-              </div>
-              <Transition
-                as={Fragment}
-                leave="transition ease-in duration-100"
-                leaveFrom="opacity-100"
-                leaveTo="opacity-0"
-                afterLeave={() => setQuery("")}
-              >
-                <Combobox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                  {filteredcategory?.length === 0 && query !== "" ? (
-                    <div className="relative cursor-default select-none py-2 px-4 text-gray-700">
-                      Nothing found.
-                    </div>
-                  ) : (
-                    filteredcategory?.map((person) => (
-                      <Combobox.Option
-                        key={person.id}
-                        className={({ active }) =>
-                          `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                            active ? "bg-teal-600 text-white" : "text-gray-900"
-                          }`
-                        }
-                        value={person}
-                      >
-                        {({ selected, active }) => (
-                          <>
-                            <span
-                              className={`block truncate ${
-                                selected ? "font-medium" : "font-normal"
-                              }`}
-                            >
-                              {person.name}
-                            </span>
-                            {selected ? (
-                              <span
-                                className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
-                                  active ? "text-white" : "text-teal-600"
-                                }`}
-                              >
-                                <svg
-                                  xmlns="http://www.w3.org/2000/svg"
-                                  fill="none"
-                                  viewBox="0 0 24 24"
-                                  strokeWidth={1.5}
-                                  stroke="currentColor"
-                                  className="h-6 w-6"
-                                  aria-hidden="true"
-                                >
-                                  <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M4.5 12.75l6 6 9-13.5"
-                                  />
-                                </svg>
-                              </span>
-                            ) : null}
-                          </>
-                        )}
-                      </Combobox.Option>
-                    ))
-                  )}
-                </Combobox.Options>
-              </Transition>
-            </div>
-          </Combobox>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Autocomplete
+              options={localCategories}
+              {...register("category")}
+              name="category"
+              selected={selectedCategory}
+              label="Category"
+              onChange={handleCategoryChange}
+            />
+            <input type="submit" />
+          </form>
         </div>
       </div>
     </div>
@@ -143,26 +61,26 @@ const Home: NextPage = () => {
 
 export default Home;
 
-const AuthShowcase: React.FC = () => {
-  const { data: sessionData } = useSession();
+/* const AuthShowcase: React.FC = () => {
+	const { data: sessionData } = useSession();
 
-  const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
-    undefined, // no input
-    { enabled: sessionData?.user !== undefined }
-  );
+	const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery(
+		undefined, // no input
+		{ enabled: sessionData?.user !== undefined }
+	);
 
-  return (
-    <div className="flex flex-col items-center justify-center gap-4">
-      <p className="text-center text-2xl text-white">
-        {sessionData && <span>Logged in as {sessionData.user?.name}</span>}
-        {secretMessage && <span> - {secretMessage}</span>}
-      </p>
-      <button
-        className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-        onClick={sessionData ? () => signOut() : () => signIn()}
-      >
-        {sessionData ? "Sign out" : "Sign in"}
-      </button>
-    </div>
-  );
-};
+	return (
+		<div className="flex flex-col items-center justify-center gap-4">
+			<p className="text-center text-2xl text-white">
+				{sessionData && <span>Logged in as {sessionData.user?.name}</span>}
+				{secretMessage && <span> - {secretMessage}</span>}
+			</p>
+			<button
+				className="rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+				onClick={sessionData ? () => signOut() : () => signIn()}
+			>
+				{sessionData ? "Sign out" : "Sign in"}
+			</button>
+		</div>
+	);
+}; */
