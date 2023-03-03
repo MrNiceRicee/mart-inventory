7bab6e71382baa316d9916e1b887d3a3a3d75ec6
Stoyko Tolev <st.tolev@gmail.com>
2023-03-03 23:52:25 +0200

    feat: Add routes to the layout component. Create the products page.

---
+++ src/pages/products.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import Layout from "../components/Layout";
+
+const Products: NextPage = () => {
+  return (
+    <Layout>
+      <h3>This is the products page</h3>
+    </Layout>
+  );
+};
+
+export default Products;
