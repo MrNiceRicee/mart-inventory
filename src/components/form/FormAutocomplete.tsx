ebb98ac1943bb8dad2e89743258a04ecddcc9ef9
stoykotolevsenteca <stoyko.tolev@sentecacommerce.com>
2023-04-24 23:30:56 +0300

    feat: rework the autocomplete to better fit my needs.

--- src/components/form/FormAutocomplete.tsx
+++ src/components/form/FormAutocomplete.tsx
@@ -1,20 +1,13 @@
-import * as React from "react";
-import { Check, ChevronsUpDown } from "lucide-react";
+import { useState } from "react";
+import { Check } from "lucide-react";
 
 import { cn } from "@/lib/utils";
-import { Button } from "@/components/ui/button";
 import {
   Command,
-  CommandEmpty,
-  CommandGroup,
+  CommandList,
   CommandInput,
   CommandItem,
 } from "@/components/ui/command";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 
 import { FormInputProps } from "./FormTextInput";
 import { useFormikContext } from "formik";
@@ -28,55 +21,44 @@ const FormAutocomplete = <T extends { id: string; name: string }>({
   name,
   placeholder = "",
 }: FormAutocompleteProps<T>) => {
-  const [open, setOpen] = React.useState(false);
+  const [open, setOpen] = useState(false);
   const { getFieldProps, setFieldValue } = useFormikContext<T>();
   const selected: T = getFieldProps(name).value;
 
   return (
     <div className="my-4">
-      <Popover open={open} onOpenChange={setOpen}>
-        <PopoverTrigger asChild>
-          <Button
-            variant="outline"
-            role="combobox"
-            aria-expanded={open}
-            className="w-[200px] justify-between"
-          >
-            {selected?.name
-              ? data.find((element) => element.id === selected.id)?.name
-              : placeholder}
-            <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
-          </Button>
-        </PopoverTrigger>
-        <PopoverContent className="w-[200px] p-0">
-          <Command>
-            <CommandInput placeholder={placeholder} />
-            <CommandEmpty>No framework found.</CommandEmpty>
-            <CommandGroup>
-              {data.map((element) => (
-                <CommandItem
-                  key={element.id}
-                  onSelect={(currentValue) => {
-                    const selected = data.find(
-                      (category) => category.name.toLowerCase() === currentValue
-                    );
-                    setFieldValue(name, selected);
-                    setOpen(false);
-                  }}
-                >
-                  <Check
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      selected?.id === element.id ? "opacity-100" : "opacity-0"
-                    )}
-                  />
-                  {element.name}
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </Command>
-        </PopoverContent>
-      </Popover>
+      <Command>
+        <CommandInput
+          placeholder={placeholder}
+          value={selected?.name}
+          onValueChange={() => {
+            setOpen(true);
+          }}
+        />
+        <CommandList>
+          {open &&
+            data.map((element) => (
+              <CommandItem
+                key={element.id}
+                onSelect={(currentValue) => {
+                  const selected = data.find(
+                    (category) => category.name.toLowerCase() === currentValue
+                  );
+                  setFieldValue(name, selected);
+                  setOpen(false);
+                }}
+              >
+                <Check
+                  className={cn(
+                    "mr-2 h-4 w-4",
+                    selected?.id === element.id ? "opacity-100" : "opacity-0"
+                  )}
+                />
+                {element.name}
+              </CommandItem>
+            ))}
+        </CommandList>
+      </Command>
     </div>
   );
 };
