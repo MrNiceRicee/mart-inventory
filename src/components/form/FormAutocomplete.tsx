3bd2e54e5ae153c1c8052db0c5bceffc83722baa
Joshua Santos <JoshCSan@gmail.com>
2023-05-10 22:55:08 -0700

    - update: add filtering to data from the backend. Change how input UX works

--- src/components/form/FormAutocomplete.tsx
+++ src/components/form/FormAutocomplete.tsx
@@ -16,35 +16,51 @@ type FormAutocompleteProps<T extends { id: string; name: string }> = {
   data: T[];
 } & FormInputProps;
 
+// i'm not TOO familar with formik, but this should either get you close to your solution or at least help out :)
 const FormAutocomplete = <T extends { id: string; name: string }>({
   data,
   name,
   placeholder = "",
 }: FormAutocompleteProps<T>) => {
   const [open, setOpen] = useState(false);
+  const [query, setQuery] = useState(""); // for what the user is typing
   const { getFieldProps, setFieldValue } = useFormikContext<T>();
   const selected: T = getFieldProps(name).value;
+
+  const filteredData = data.filter((item) =>
+    // use what the user has typed to just client side filter
+    item.name.toLowerCase().includes(query.toLowerCase())
+  );
 
   return (
     <div className="my-4">
       <Command>
         <CommandInput
           placeholder={placeholder}
-          value={selected?.name}
-          onValueChange={() => {
+          onFocusCapture={() => {
+            // open the dropdown when the input is focused
             setOpen(true);
+          }}
+          value={query}
+          onChangeCapture={(e) => {
+            setQuery(e.currentTarget.value);
+          }}
+          // close input if user tabs out
+          onBlurCapture={() => {
+            setOpen(false);
           }}
         />
         <CommandList>
           {open &&
-            data.map((element) => (
+            filteredData.map((element) => (
               <CommandItem
                 key={element.id}
+                value={element.id}
                 onSelect={(currentValue) => {
-                  const selected = data.find(
-                    (category) => category.name.toLowerCase() === currentValue
-                  );
-                  setFieldValue(name, selected);
+                  // currentValue will be the value of the selected item (element.id)
+                  setFieldValue(name, currentValue);
+                  // set the query to update (because the query is also the display value)
+                  setQuery(element.name);
                   setOpen(false);
                 }}
               >
